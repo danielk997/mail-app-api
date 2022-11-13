6d9cd79749b33121b67f7e110d087eb6053548ae
Daniel Kociolek <daniel.kociolek@hawatel.com>
2022-11-13 10:48:13 +0100

    Fix okta auth

--- src/main/java/com/mailapp/mailapi/modules/campaigns/web/CampaignController.java
+++ src/main/java/com/mailapp/mailapi/modules/campaigns/web/CampaignController.java
@@ -1,14 +1,14 @@
 package com.mailapp.mailapi.modules.campaigns.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("campaigns")
 public class CampaignController {
 
-    @GetMapping("/")
+    @GetMapping
     public String getTest() {
         return "Test123";
     }
