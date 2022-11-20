f8a30f5dce1594cdbe0d93674cc46e9251f19a50
Daniel Kociolek <daniel.kociolek@hawatel.com>
2022-11-20 17:46:56 +0100

    Add mail sender

--- src/main/java/com/mailapp/mailapi/modules/campaigns/web/ViewController.java
+++ src/main/java/com/mailapp/mailapi/modules/campaigns/web/ViewController.java
@@ -1,13 +1,16 @@
 package com.mailapp.mailapi.modules.campaigns.web;
 
-import com.mailapp.mailapi.modules.campaigns.dto.SentCampaignDTO;
+import com.mailapp.mailapi.mail.MailSender;
 import com.mailapp.mailapi.modules.campaigns.dto.ViewAddDTO;
 import com.mailapp.mailapi.modules.campaigns.dto.ViewDTO;
-import com.mailapp.mailapi.modules.campaigns.model.View;
 import com.mailapp.mailapi.modules.campaigns.service.ViewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
@@ -31,6 +34,8 @@ public class ViewController {
     public ViewAddDTO addView(HttpServletRequest http, @RequestParam("mail") String email, @RequestParam("uuid") UUID parentId) {
         System.out.println(http);
 
+        sendMessage();
+
         ViewAddDTO dto = ViewAddDTO.builder()
                 .id(UUID.randomUUID())
                 .email(email)
@@ -40,4 +45,24 @@ public class ViewController {
 
         return viewService.add(dto);
     }
+
+    private void sendMessage() {
+        JavaMailSenderImpl emailSender = MailSender.getSender();
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        String content = "<h2 style=\"color: red\">Test 123</h2>";
+
+        try {
+            helper.setFrom("test@vp.pl");
+            helper.setTo("danielkociolek@vp.pl");
+            helper.setSubject("Wiadomość test 1");
+            helper.setText(content, true);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        emailSender.send(message);
+    }
 }
+//a0c8c5508ca627c2e6eab5b10d2ff336-2de3d545-59e55d55
